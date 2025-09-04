fdfea2b0ac44e10ef00878adff6945fb4fce3fcb
ARTEM POBEHAILO <partemishe@gmail.com>
2025-09-04 19:56:41 +0300

    do

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import "./globals.css";
 
 import { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import Head from "next/head";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -47,15 +46,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.variable}>
         <TanStackProvider>
-          <Head>
-            <meta
-              name="description"
-              content="View the notes filtered by: All"
-            />
-            <meta name="robots" content="index, follow" />
-            <title>NoteHub is a convenient note manager</title>
-          </Head>
-
           <Header />
 
           {children}
